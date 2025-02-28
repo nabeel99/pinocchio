ebbf623a8773d541ce86d2bf8c4f1fc22ba44bf8
aaran <nabeelnaveed98@icloud.com>
2025-02-28 07:24:19 +0500

    add : token2022

---
+++ programs/token-2022/src/instructions/revoke.rs
@@ -0,0 +1,41 @@
+use pinocchio::{
+    account_info::AccountInfo,
+    instruction::{AccountMeta, Instruction, Signer},
+    program::invoke_signed,
+    ProgramResult,
+};
+
+/// Revokes the delegate's authority.
+///
+/// ### Accounts:
+///   0. `[WRITE]` The source account.
+///   1. `[SIGNER]` The source account owner.
+pub struct Revoke<'a> {
+    /// Source Account.
+    pub source: &'a AccountInfo,
+    ///  Source Owner Account.
+    pub authority: &'a AccountInfo,
+}
+
+impl Revoke<'_> {
+    #[inline(always)]
+    pub fn invoke(&self) -> ProgramResult {
+        self.invoke_signed(&[])
+    }
+
+    pub fn invoke_signed(&self, signers: &[Signer]) -> ProgramResult {
+        // account metadata
+        let account_metas: [AccountMeta; 2] = [
+            AccountMeta::writable(self.source.key()),
+            AccountMeta::readonly_signer(self.authority.key()),
+        ];
+
+        let instruction = Instruction {
+            program_id: &crate::ID,
+            accounts: &account_metas,
+            data: &[5],
+        };
+
+        invoke_signed(&instruction, &[self.source, self.authority], signers)
+    }
+}
